14756dd9cc6f232532c99537c2cdc9ad9c482ad0
Caroline <cahumada@skillstorm.com>
2022-08-25 04:20:33 -0400

    updated files

--- Example-1/src/script.js
+++ Example-1/src/script.js
@@ -3,6 +3,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Loading
+const textureLoader = new THREE.TextureLoader()
+
+const normalTexture = textureLoader.load('/textures/NormalMap.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,16 +18,22 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+//const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.SphereBufferGeometry(.5, 64, 64)
 
 // Materials
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+material.normalMap = normalTexture
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+material.color = new THREE.Color(0x292929)
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
 scene.add(sphere)
+
+gui.add(material, 'wireframe')
 
 // Lights
 
@@ -30,7 +41,66 @@ const pointLight = new THREE.PointLight(0xffffff, 0.1)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
+
 scene.add(pointLight)
+
+// Light 2 red
+const pointLight2 = new THREE.PointLight(0xff0000, 2)
+pointLight2.position.set(-2.4,1.3,-0.9)
+pointLight2.intensity = 10
+
+scene.add(pointLight2)
+
+const light2 = gui.addFolder('Light 2')
+
+// when you specify min and max you get the slider option 
+light2.add(pointLight2.position, 'x').min(-6).max(6).step(0.1)
+light2.add(pointLight2.position, 'y').min(-3).max(3).step(0.1)
+light2.add(pointLight2.position, 'z').min(-3).max(3).step(0.1)
+light2.add(pointLight2, 'intensity').min(0).max(10).step(0.1)
+
+const light2Color = {
+    color: 0xff0000
+}
+
+// creates a color pallet for pointLight3
+// dat.GUI will notify THREEjs to change the pointLight2 color
+light2.addColor(light2Color, 'color')
+    .onChange(() => {
+        pointLight2.color.set(light2Color.color)
+    })
+
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight2, 1)
+// scene.add(pointLightHelper2)
+
+// Light 3 blue
+const pointLight3 = new THREE.PointLight(0x3ef7, 2)
+pointLight3.position.set(2,-1.9,0.6)
+pointLight3.intensity = 7.5
+
+scene.add(pointLight3)
+
+const light3 = gui.addFolder('Light 3')
+
+// when you specify min and max you get the slider option 
+light3.add(pointLight3.position, 'x').min(-6).max(6).step(0.1)
+light3.add(pointLight3.position, 'y').min(-3).max(3).step(0.1)
+light3.add(pointLight3.position, 'z').min(-3).max(3).step(0.1)
+light3.add(pointLight3, 'intensity').min(0).max(10).step(0.1)
+
+const light3Color = {
+    color: 0x3ef7
+}
+
+// creates a color pallet for pointLight3
+// dat.GUI will notify THREEjs to change the pointLight3 color
+light3.addColor(light3Color, 'color')
+    .onChange(() => {
+        pointLight3.color.set(light3Color.color)
+    })
+
+// const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, 1)
+// scene.add(pointLightHelper3)
 
 /**
  * Sizes
@@ -73,7 +143,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -82,15 +153,37 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+document.addEventListener('mousemove', onDocumentMouseMove)
+
+let mouseX = 0
+let mouseY = 0
+
+let targetX = 0
+let targetY = 0
+
+const windowX = window.innerWidth / 2
+const windowY = window.innerHeight / 2
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowX)
+    mouseY = (event.clientY - windowY)
+}
+
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
+    targetX = mouseX * .001
+    targetY = mouseY * .001
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+
+    sphere.rotation.x += .05 * (targetY - sphere.rotation.x)
+    sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
+    sphere.position.z += -.05 * (targetY - sphere.rotation.x)
 
     // Update Orbital Controls
     // controls.update()
